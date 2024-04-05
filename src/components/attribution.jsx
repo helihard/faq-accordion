9d63d2a54178eaaecf413387d8d1e824ca90284e
Heli Hård Källoff <vierge.hard@gmail.com>
2024-04-05 13:04:42 +0200

    starter from Frontend Mentor

---
+++ src/components/attribution.jsx
@@ -0,0 +1,15 @@
+function Attribution() {
+  return (
+    <>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  );
+}
+
+export default Attribution;
