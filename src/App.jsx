23d87bb6693043be0d8fc3a95d1039fe98100511
Heli Hård Källoff <vierge.hard@gmail.com>
2024-04-08 13:29:47 +0200

    moved background image styling from css --> inline-styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import MainContainer from "./components/MainContainer";
 import Attribution from "./components/Attribution";
+import backgroundPatternDesktop from "./assets/images/background-pattern-desktop.svg";
+import backgroundPatternMobile from "./assets/images/background-pattern-mobile.svg";
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState(
+    window.innerWidth <= 375
+      ? backgroundPatternMobile
+      : backgroundPatternDesktop
+  );
+
+  useEffect(() => {
+    const handleBrowserResize = () => {
+      setBackgroundImage(
+        window.innerWidth <= 375
+          ? backgroundPatternMobile
+          : backgroundPatternDesktop
+      );
+    };
+
+    window.addEventListener("resize", handleBrowserResize);
+
+    return () => {
+      window.removeEventListener("resize", handleBrowserResize);
+    };
+  }, []);
+
   return (
     <>
-      <MainContainer />
-      <Attribution />
+      <div
+        id="body-container"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "top center",
+          display: "grid",
+          placeItems: "start center",
+        }}
+      >
+        <MainContainer />
+        <Attribution />
+      </div>
     </>
   );
 }
